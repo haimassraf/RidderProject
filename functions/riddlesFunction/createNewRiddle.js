5d1c01d55260dc1063e141144933fe2ab1e5cbc8
Haim Assraf <haimassraf@gmail.com>
2025-07-09 13:32:47 +0300

    df

--- functions/riddlesFunction/createNewRiddle.js
+++ functions/riddlesFunction/createNewRiddle.js
@@ -1,27 +1,37 @@
 import readline from 'readline-sync';
-import { createRiddle } from '../../../server/controllers/riddleController.js';
 
-export function createNewRiddle() {
-    const newRiddle = {
-    }
+export async function createNewRiddle() {
+    try {
+        const newRiddle = {
+        }
 
-    newRiddle.name = requiredQuestion("Name: ")
-    newRiddle.taskDescription = requiredQuestion("Task description: ");
-    const isMultiple = readline.question("Multiple choices? (y) ").toLowerCase();
-    if (isMultiple === 'y') {
-        newRiddle.choices = addMultipleAnswer();
-        newRiddle.correctAnswer = requiredQuestion("correct answer (1-4): ", false, true);
+        newRiddle.name = requiredQuestion("Name: ")
+        newRiddle.taskDescription = requiredQuestion("Task description: ");
+        const isMultiple = readline.question("Multiple choices? (y) ").toLowerCase();
+        if (isMultiple === 'y') {
+            newRiddle.choices = addMultipleAnswer();
+            newRiddle.correctAnswer = requiredQuestion("correct answer (1-4): ", false, true);
+        }
+        else {
+            newRiddle.correctAnswer = requiredQuestion("correct Answer: ")
+        }
+        newRiddle.difficulty = requiredQuestion("difficulty (easy, medium, hard): ", true);
+        newRiddle.hint = readline.question("Hint (or enter if not): ");
+        const isTimeLimit = readline.question("Time Limit? (y) ").toLowerCase();
+        if (isTimeLimit === 'y') {
+            newRiddle.timeLimit = requiredQuestion("Time Limit: ", false, false, true);
+        }
+        const res =  await fetch("http://localhost:3000/riddle", {
+            method: 'POST',
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(newRiddle)
+        }).then((res) => res.json())
+        console.log(res)
+    } catch (err) {
+        console.log("Error: ", err);
     }
-    else {
-        newRiddle.correctAnswer = requiredQuestion("correct Answer: ")
-    }
-    newRiddle.difficulty = requiredQuestion("difficulty (easy, medium, hard): ", true);
-    newRiddle.hint = readline.question("Hint (or enter if not): ");
-    const isTimeLimit = readline.question("Time Limit? (y) ").toLowerCase();
-    if (isTimeLimit === 'y') {
-        newRiddle.timeLimit = requiredQuestion("Time Limit: ", false, false, true);
-    }
-    createRiddle(newRiddle);
 }
 
 function requiredQuestion(question, isDifficulty = false, isMultiple = false, timeLimit = false) {
